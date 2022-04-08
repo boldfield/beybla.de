f39204092b0bb4b8fc94cf62d6f6614abcb9712c
Brian Oldfield <brian@oldfield.io>
2022-04-07 23:56:16 -0700

    Fix mixpanel...

--- site/scripts/newsite.js
+++ site/scripts/newsite.js
@@ -4,7 +4,6 @@ $.BEYBLADE = {
   metadata: {},
   supportedStates: [],
   chart: null,
-  mixpanel: null,
   fetchSupportedStates: function() {
     this.supportedStates = ["CA", "WA"];
   },
@@ -41,8 +40,8 @@ $.BEYBLADE = {
   refreshDisplay: function() {
     var state = this.selectedState;
     location.hash = "#" + state;
-    if (!!this.mixpanel) {
-      this.mixpanel.track('load-' + state);
+    if (location.host == "beybla.de") {
+      mixpanel.track('load-' + state);
     }
 
     $(".state-label").text(this.metadata[state]["stateLabel"]);
@@ -133,7 +132,7 @@ $.BEYBLADE = {
     });
   },
   setupMixpanel: function() {
-    if (!!this.mixpanel || (location.host != "beybla.de")) {
+    if (location.host != "beybla.de") {
       return;
     }
     (function(f,b){if(!b.__SV){var e,g,i,h;window.mixpanel=b;b._i=[];b.init=function(e,f,c){function g(a,d){var b=d.split(".");2==b.length&&(a=a[b[0]],d=b[1]);a[d]=function(){a.push([d].concat(Array.prototype.slice.call(arguments,0)))}}var a=b;"undefined"!==typeof c?a=b[c]=[]:c="mixpanel";a.people=a.people||[];a.toString=function(a){var d="mixpanel";"mixpanel"!==c&&(d+="."+c);a||(d+=" (stub)");return d};a.people.toString=function(){return a.toString(1)+".people (stub)"};i="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
@@ -141,7 +140,6 @@ $.BEYBLADE = {
     MIXPANEL_CUSTOM_LIB_URL:"file:"===f.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";g=f.getElementsByTagName("script")[0];g.parentNode.insertBefore(e,g)}})(document,window.mixpanel||[]);
     mixpanel.init('574994a080b8cdb4d4f5b82ef68f5c44');
     mixpanel.track('load-index');
-    this.mixpanel = mixpanel;
   }
 }
 
